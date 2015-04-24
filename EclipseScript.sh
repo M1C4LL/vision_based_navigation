8a61373258b918bf9fb35b5a6ebfa89ad780cb50
Michael Bunz <Michael.Bunz@gmail.com>
2015-04-24 13:56:21 +0200

    added shell script to make eclipse project

--- EclipseScript.sh
+++ EclipseScript.sh
@@ -1,3 +1,7 @@
+# Script according to ROS IDEs chapter 2.3 catkin tools on 
+# http://wiki.ros.org/IDEs#Building_the_project_inside_Eclipse
+# To generate eclipse-project you need to execute: 
+
 ROOT=$PWD 
 cd build
 for PROJECT in `find $PWD -name .project`; do
